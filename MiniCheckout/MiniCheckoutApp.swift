ed263bb44255399b89a1f5752a5e2376ec6a88db
Damien Babington <damien.babington@gmail.com>
2025-12-18 09:24:55 +0900

    Initial Commit

---
+++ MiniCheckout/MiniCheckoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniCheckoutApp.swift
+//  MiniCheckout
+//
+//  Created by Damien Babington on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MiniCheckoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
